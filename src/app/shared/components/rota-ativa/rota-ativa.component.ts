e99ced6184c854a31adbc9178bb1eb2b7850878a
Diego Bonfim de Santana <diego.bonfim.dev@gmail.com>
2025-03-03 21:21:41 -0300

    Ajuste totaAtiva

--- src/app/shared/components/rota-ativa/rota-ativa.component.ts
+++ src/app/shared/components/rota-ativa/rota-ativa.component.ts
@@ -9,7 +9,7 @@ import { filter } from 'rxjs';
 })
 export class RotaAtivaComponent {
   actualRoute: string = '';
-
+  iconClass: string = ''; // Ícone padrão
   constructor(private router: Router) {}
 
   ngOnInit() {
@@ -21,16 +21,22 @@ export class RotaAtivaComponent {
 
         if (urlAtual.includes('home')) {
           this.actualRoute = 'Home';
+          this.iconClass = 'home';
         } else if (urlAtual.includes('about')) {
           this.actualRoute = 'About';
+          this.iconClass = 'badge';
         } else if (urlAtual.includes('projects')) {
           this.actualRoute = 'Projects';
+          this.iconClass = 'work';
         } else if (urlAtual.includes('services')) {
           this.actualRoute = 'Service';
+          this.iconClass = 'design_services';
         } else if (urlAtual.includes('contact')) {
           this.actualRoute = 'Contact';
+          this.iconClass = 'alternate_email';
         } else {
           this.actualRoute = 'Home';
+          this.iconClass = 'home';
         }
 
         console.log('URL atual:', urlAtual);
