9e72954aaef078a9c7a8620be8569fcdd2f89d79
Diego Bonfim de Santana <diego.bonfim.dev@gmail.com>
2025-04-13 13:11:35 -0300

    Inicio page about

--- src/app/pages/components/sobre/sobre.component.ts
+++ src/app/pages/components/sobre/sobre.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
-  styleUrls: ['./sobre.component.css']
+  styleUrls: ['./sobre.component.css'],
 })
 export class SobreComponent {
-
+  me1: string = 'assets/images/me1.png';
+  me1mobile: string = 'assets/images/me1mobile.png';
 }
