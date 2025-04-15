6084fc05ce1499fa4886d86f4fc85bc2e5ef51b5
Diego Bonfim de Santana <diego.bonfim.dev@gmail.com>
2025-04-15 19:48:21 -0300

    Ajuste rota-ativa

--- src/app/pages/components/sobre/sobre.component.ts
+++ src/app/pages/components/sobre/sobre.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./sobre.component.css'],
 })
 export class SobreComponent {
+  rotaAtual: string = 'SOBRE';
   me1mobile: string = 'assets/images/me1mobile.png';
   serpro: string = 'assets/images/serpro.png';
 }
--- src/app/shared/components/rota-ativa/rota-ativa.component.ts
+++ src/app/shared/components/rota-ativa/rota-ativa.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
 
@@ -8,38 +8,11 @@ import { filter } from 'rxjs';
   styleUrls: ['./rota-ativa.component.css'],
 })
 export class RotaAtivaComponent {
-  actualRoute: string = '';
-  iconClass: string = ''; // Ícone padrão
+  @Input() actualRoute: string = '';
+  @Input() backPage!: () => void;
+  @Input() nextPage!: () => void;
+
   constructor(private router: Router) {}
 
-  ngOnInit() {
-    // Escutando mudanças na URL
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe((event: any) => {
-        const urlAtual = event.urlAfterRedirects;
-
-        if (urlAtual.includes('home')) {
-          this.actualRoute = 'Home';
-          this.iconClass = 'home';
-        } else if (urlAtual.includes('about')) {
-          this.actualRoute = 'About';
-          this.iconClass = 'badge';
-        } else if (urlAtual.includes('projects')) {
-          this.actualRoute = 'Projects';
-          this.iconClass = 'work';
-        } else if (urlAtual.includes('services')) {
-          this.actualRoute = 'Service';
-          this.iconClass = 'design_services';
-        } else if (urlAtual.includes('contact')) {
-          this.actualRoute = 'Contact';
-          this.iconClass = 'alternate_email';
-        } else {
-          this.actualRoute = 'Home';
-          this.iconClass = 'home';
-        }
-
-        console.log('URL atual:', urlAtual);
-      });
-  }
+  ngOnInit() {}
 }
