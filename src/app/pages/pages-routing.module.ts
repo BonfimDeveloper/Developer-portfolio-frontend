f80b4a8d4072ab90b82cc4f25f761cb5f80d672b
Diego Bonfim de Santana <diego.bonfim.dev@gmail.com>
2025-06-22 13:02:20 -0300

    Route adjust

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,21 +1,10 @@
-import { PagesModule } from './pages.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { SobreComponent } from './components/sobre/sobre.component';
-import { ProjetosComponent } from './components/projetos/projetos.component';
-import { ServicosComponent } from './components/servicos/servicos.component';
-import { ContatoComponent } from './components/contato/contato.component';
 import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/pages/my-port', pathMatch: 'full' }, // Redireciona de '' para '/pages/home'
-  { path: 'my-port', component: MainComponent },
-
-  { path: 'about', component: SobreComponent },
-  { path: 'projects', component: ProjetosComponent },
-  { path: 'services', component: ServicosComponent },
-  { path: 'contact', component: ContatoComponent },
+  { path: '', redirectTo: '/pages/my-portifolio', pathMatch: 'full' },
+  { path: 'my-portifolio', component: MainComponent },
 ];
 
 @NgModule({
